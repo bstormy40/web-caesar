b66e5dcbe599fb62546d006fa711a8275a6d9b9b
Blake <chrx@chrx.com>
2017-03-27 20:36:26 -0500

    Initial commit; displays encrypted 'Hello world' message

---
+++ caesar.py
@@ -0,0 +1,28 @@
+import string
+def alphabet_position(char):
+    newchar = str.lower(char)
+    CharIndex = alphabet.index(newchar)
+    return CharIndex
+def rotate_character(char, rot):
+    if char.isalpha() == True:
+        letterIndex = alphabet_position(char)
+        rotatedIndex = letterIndex + rot
+        if rotatedIndex < 26:
+            encrypted = alphabet[rotatedIndex]
+        else:
+            encrypted = alphabet[rotatedIndex % 26]
+        if char.isupper() == True:
+            return encrypted.upper()
+        else:
+            return encrypted
+    return char
+alphabet = string.ascii_lowercase
+
+def encrypt(text, rot):
+    encryption = ''
+    for char in text:
+        if char == ' ':
+            encryption = encryption + ' '
+        else:
+            encryption = encryption + rotate_character(char, rot)
+    return encryption
